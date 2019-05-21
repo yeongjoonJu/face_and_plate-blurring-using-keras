ba60d362776f256c4617e9f02eb871bf24dbe163
yeongjoon <go1217jo@naver.com>
2019-05-21 15:16:12 +0900

    feat: face recognition using haar

---
+++ face_recognition/recognition.py
@@ -0,0 +1,69 @@
+from . import model
+import glob
+import os
+import cv2
+import numpy as np
+
+def create_input_image_embeddings():
+    input_embeddings = {}
+    inception_model = model.create_model()
+    for file in glob.glob("images/*"):
+        person_name = os.path.splitext(os.path.basename(file))[0]
+        image_file = cv2.imread(file, 1)
+        input_embeddings[person_name] = image_to_embedding(image_file, inception_model)
+
+    return input_embeddings, inception_model
+
+
+def image_to_embedding(image, model):
+    #image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_AREA) 
+    image = cv2.resize(image, (96, 96)) 
+    img = image[...,::-1]
+    img = np.around(np.transpose(img, (0,1,2))/255.0, decimals=12)
+    x_train = np.array([img])
+    embedding = model.predict_on_batch(x_train)
+    return embedding
+
+
+def recognize_face(face_image, input_embeddings, model):
+    embedding = image_to_embedding(face_image, model)
+    
+    minimum_distance = 200
+    name = None
+    
+    # Loop over  names and encodings.
+    for (input_name, input_embedding) in input_embeddings.items():       
+        euclidean_distance = np.linalg.norm(embedding-input_embedding)
+        print('Euclidean distance from %s is %s' %(input_name, euclidean_distance))
+        if euclidean_distance < minimum_distance:
+            minimum_distance = euclidean_distance
+            name = input_name
+    
+    if minimum_distance < 0.68:
+        return str(name)
+    else:
+        return None
+
+
+def detect_face(image, input_embeddings, model, face_detector, font=cv2.FONT_HERSHEY_SIMPLEX):
+    height, width, cmap = image.shape
+    gray = cv2.cvtColor(cv2.UMat(image), cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    # Loop through all the faces detected
+    identities = []
+    
+    for (x, y, w, h) in faces:
+        x1 = x
+        y1 = y
+        x2 = x+w
+        y2 = y+h
+
+        face_image = image[max(0, y1):min(height, y2), max(0, x1):min(width, x2)]
+        identity = recognize_face(face_image, input_embeddings, model)
+
+        if identity is not None:
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(image, str(identity), (x1+5, y1-5), font, 1, (255,255,255), 2)
+    
+    return image
